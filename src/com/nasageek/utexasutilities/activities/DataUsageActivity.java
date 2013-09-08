c252981c45253eaa2047096f4aa63d6581dac007
Chris Roberts <randomnameftw@gmail.com>
2013-09-07 22:38:18 -0500

    trim down DataUsageActivity.java, add .gitignore for real this time?

--- src/com/nasageek/utexasutilities/activities/DataUsageActivity.java
+++ src/com/nasageek/utexasutilities/activities/DataUsageActivity.java
@@ -55,7 +55,7 @@ import com.nasageek.utexasutilities.R;
 import com.nasageek.utexasutilities.R.id;
 import com.nasageek.utexasutilities.R.layout;
 
-public class DataUsageActivity extends SherlockActivity implements OnTouchListener {
+public class DataUsageActivity extends SherlockActivity {
 	
 	private DefaultHttpClient httpclient;
 	private SharedPreferences settings;
@@ -81,14 +81,9 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 	
 	private double percentused;
  
-	final private double difPadding = 0.1;
-	
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	//	pd = ProgressDialog.show(DataUsageActivity.this, "", "Loading...");
-		
 		setContentView(R.layout.data_layout);
 
 		d_pb_ll = (LinearLayout) findViewById(R.id.data_progressbar_ll);
@@ -96,8 +91,6 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 		mProgress = (ProgressBar) findViewById(R.id.percentDataUsed);
 		dell = (LinearLayout) findViewById(R.id.data_error);
 		detv = (TextView) findViewById(R.id.tv_failure);
-//		mProgress.setBackgroundColor(Color.rgb(204,85,0));
-		
 		actionbar = getSupportActionBar();
 		actionbar.setTitle("Data Usage");
 		actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -107,15 +100,11 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 		graph = (XYPlotZoomPan) findViewById(R.id.mySimpleXYPlot);
 		
 		((XYPlotZoomPan)graph).setZoomVertically(false);
-		//graph.setOnTouchListener(this);
 		graph.setMarkupEnabled(false);
-
-
 		settings = PreferenceManager.getDefaultSharedPreferences(this);
 		
-		labels = new Long[288];//2017];
-		downdata = new Float[288];//2017];
-	//	updata = new Float[2017];
+		labels = new Long[288];
+		downdata = new Float[288];
 		totaldata = new Float[288];
 		
 		httpclient = ConnectionHelper.getThreadSafeClient();
@@ -128,125 +117,6 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 		
 	}
 	
-	
-	// Definition of the touch states
-	static final private int NONE = 0;
-	static final private int ONE_FINGER_DRAG = 1;
-	static final private int TWO_FINGERS_DRAG = 2;
-	private int mode = NONE;
- 
-	private PointF firstFinger;
-	private float lastScrolling;
-	private float distBetweenFingers;
-	private float lastZooming;
- 
-	
-	public boolean onTouch(View arg0, MotionEvent event) {
-		switch(event.getAction() & MotionEvent.ACTION_MASK) {
-			case MotionEvent.ACTION_DOWN: // Start gesture
-				firstFinger = new PointF(event.getX(), event.getY());
-				mode = ONE_FINGER_DRAG;
-				break;
-			case MotionEvent.ACTION_UP:
-			case MotionEvent.ACTION_POINTER_UP:
-				//When the gesture ends, a thread is created to give inertia to the scrolling and zoom 
-				final Timer t = new Timer();
-				t.schedule(new TimerTask() {
-					@Override
-					public void run() {
-						while(Math.abs(lastScrolling) > 1f || Math.abs(lastZooming - 1) > 1.01) {
-							lastScrolling *= .8;	//speed of scrolling damping
-							scroll(lastScrolling);
-							lastZooming += (1 - lastZooming) * .2;	//speed of zooming damping
-							zoom(lastZooming);
-							graph.redraw();
-						}
-					}
-				}, 0);break;
- 
-		/*	case MotionEvent.ACTION_POINTER_DOWN: // second finger
-				distBetweenFingers = spacing(event);
-				// the distance check is done to avoid false alarms
-				if (distBetweenFingers > 5f)
-					mode = TWO_FINGERS_DRAG;
-				break;*/
-			case MotionEvent.ACTION_MOVE:
-				if (mode == ONE_FINGER_DRAG) {
-					final PointF oldFirstFinger = firstFinger;
-					firstFinger = new PointF(event.getX(), event.getY());
-					lastScrolling = oldFirstFinger.x - firstFinger.x;
-					scroll(lastScrolling);
-					lastZooming = (firstFinger.y - oldFirstFinger.y) / graph.getHeight();
-					if (lastZooming < 0)
-						lastZooming = 1 / (1 - lastZooming);
-					else
-						lastZooming += 1;
-					zoom(lastZooming);
-					
-					graph.redraw();
- 
-				}/*else if (mode == TWO_FINGERS_DRAG) {
-					final float oldDist = distBetweenFingers;
-					distBetweenFingers = spacing(event);
-					if(distBetweenFingers > 0)
-					{	lastZooming = oldDist / distBetweenFingers;
-						zoom(lastZooming);
-						checkBoundaries();
-						graph.redraw();}
-					else break;
-				}*/
-				break;
-		}
-		return true;
-	}
- 
-	private void zoom(float scale) {
-		final double domainSpan = maxXY.x - minXY.x;
-		final double domainMidPoint = maxXY.x - domainSpan / 2.0f;
-		final double offset = domainSpan * scale / 2.0f;
-		minXY.x = domainMidPoint - offset;
-		maxXY.x = domainMidPoint + offset;
-		checkBoundaries();
-		
-	}
- 
-	private void scroll(float pan) {
-		final double domainSpan = maxXY.x - minXY.x;
-		final double step = domainSpan / graph.getWidth();
-		final double offset = pan * step;
-		minXY.x += offset;
-		maxXY.x += offset;
-
-
-		if (minXY.x < absMinX)
-		{	minXY.x = absMinX;
-			maxXY.x -= offset;//(absMinX-minXY.x);
-			}
-		else if (minXY.x > maxNoError)
-			minXY.x = maxNoError;
-		if (maxXY.x > absMaxX)
-		{	maxXY.x = absMaxX;//(maxXY.x-absMaxX);
-			minXY.x -= offset;//(maxXY.x-absMaxX);
-			}
-		else if (maxXY.x < minNoError)
-			maxXY.x = minNoError;
-		if (maxXY.x - minXY.x < minDif)
-			maxXY.x = maxXY.x + (double) (minDif - (maxXY.x - minXY.x));
-		
-		graph.setDomainBoundaries(minXY.x, maxXY.x, BoundaryMode.FIXED);
-		
-	}
- 
-	private float spacing(MotionEvent event) {
-		if(event.getPointerCount()>=2)
-		{	final float x = event.getX(0) - event.getX(1);
-			final float y = event.getY(0) - event.getY(1);
-			return FloatMath.sqrt(x * x + y * y);
-		}
-		
-		else return 1f;
-	}
- 
 	private void checkBoundaries() {
 		//Make sure the proposed domain boundaries will not cause plotting issues
 		if (minXY.x < absMinX  || ((Double)minXY.x).equals(Double.NaN))
@@ -320,7 +190,6 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 		
 		@Override
 		protected Character doInBackground(Object... params) {
-	//		DefaultHttpClient httpclient = ch.getThreadSafeClient();
 			HttpGet hget = null;
 			Pattern authidpattern = Pattern.compile("(?<=%20)\\d+");
 	    	Matcher authidmatcher = authidpattern.matcher(client.getCookieStore().getCookies().get(0).getValue());
@@ -345,7 +214,6 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 				return ' ';
 			}
 	    	
-	    	
 	    	String[] lines = pagedata.split("\n");
 	    	if(!lines[0].equals("Date,MB In,MB Out,MB Total")) {
 				cancel(true);
@@ -359,12 +227,12 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 	    		String[] entry = lines[i].split(",");
 	    		date.clear();
 	    		try {
-	    		//TODO: this crashes sometimes on the [2] access, not sure why
-	    		date.set(Integer.parseInt(entry[0].split("/")[0]),
-	    				Integer.parseInt(entry[0].split("/")[1]) - 1,
-	    				Integer.parseInt(entry[0].split("/| ")[2]),
-	    				Integer.parseInt(entry[0].split(" |:")[1]),
-	    				Integer.parseInt(entry[0].split(" |:")[2]));
+		    		//TODO: this crashes sometimes on the [2] access, not sure why
+		    		date.set(Integer.parseInt(entry[0].split("/")[0]),
+		    				Integer.parseInt(entry[0].split("/")[1]) - 1,
+		    				Integer.parseInt(entry[0].split("/| ")[2]),
+		    				Integer.parseInt(entry[0].split(" |:")[1]),
+		    				Integer.parseInt(entry[0].split(" |:")[2]));
 	    		}
 	    		catch(NumberFormatException nfe) {
 	    			cancel(true);
@@ -387,14 +255,10 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 		protected void onPostExecute(Character result) {
 			XYSeries series = new SimpleXYSeries(Arrays.asList(labels), Arrays.asList(downdata), "Downloaded");
 			XYSeries seriestotal = new SimpleXYSeries(Arrays.asList(labels), Arrays.asList(totaldata), "Uploaded");
+
+			BarFormatter downbarformatter = new BarFormatter(0xFF42D692, Color.BLACK);
+			BarFormatter upbarformatter = new BarFormatter(0xFF388DFF, Color.BLACK);
 			
-	//		LineAndPointFormatter downlineformatter = new LineAndPointFormatter(0xFFFFAD3B, 0xFFFFAD3B, 0xFFFFAD3B);
-	//		LineAndPointFormatter uplineformatter = new LineAndPointFormatter(0xFF388DFF, 0xFF388DFF, 0xFF388DFF);
-			
-			BarFormatter downbarformatter = new BarFormatter( 0xFF42D692/*FFAD3B/*Color.rgb(68,166,75)*/, Color.BLACK);
-			BarFormatter upbarformatter = new BarFormatter(0xFF388DFF/*Color.rgb(130,159,255)*/, Color.BLACK);
-			
-		//	42D692
 			downbarformatter.getBorderPaint().setStrokeWidth(0);
 			upbarformatter.getBorderPaint().setStrokeWidth(0);
 			
@@ -405,17 +269,17 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 	        
 	        graph.getGraphWidget().setDomainGridLinePaint(gridpaint);
 			graph.getGraphWidget().setRangeGridLinePaint(gridpaint);
+			graph.setTicksPerDomainLabel(4);
 
 			graph.addSeries(seriestotal, upbarformatter);
 			graph.addSeries(series, downbarformatter);
-			graph.setTicksPerDomainLabel(4);
 			
 			graph.setDomainStep(XYStepMode.INCREMENT_BY_VAL,1800000);
 			graph.setRangeStep(XYStepMode.INCREMENT_BY_VAL,30);
 			
 			graph.calculateMinMaxVals();
 			minXY = new PointD(graph.getCalculatedMinX().doubleValue(),
-					graph.getCalculatedMinY().doubleValue()); //initial minimum data point
+			graph.getCalculatedMinY().doubleValue()); //initial minimum data point
 			absMinX = minXY.x; //absolute minimum data point
 			//TODO: this crashes with a NPE sometimes.  ??
 			//absolute minimum value for the domain boundary maximum
@@ -429,29 +293,13 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 			}
 			minNoError = Math.round(temp.doubleValue() + 2);
 			maxXY = new PointD(graph.getCalculatedMaxX().doubleValue(),
-					graph.getCalculatedMaxY().doubleValue()); //initial maximum data point
+			graph.getCalculatedMaxY().doubleValue()); //initial maximum data point
 			
 			graph.setTicksPerRangeLabel(maxXY.y > 61 ? 2 : 1);
 			
 			absMaxX = maxXY.x; //absolute maximum data point
 			//absolute maximum value for the domain boundary minimum
 			maxNoError = (double) Math.round(series.getX(series.size() - 1).doubleValue()) - 2;
-	 
-			//Check x data to find the minimum difference between two neighboring domain values
-			//Will use to prevent zooming further in than this distance
-		/*	double temp1 = series.getX(0).doubleValue();
-			double temp2 = series.getX(1).doubleValue();
-			double temp3;
-			double thisDif;*/
-		/*	minDif = 1000000;	//increase if necessary for domain values
-			for (int i = 2; i < series.size(); i++) {
-				temp3 = series.getX(i).doubleValue();
-				thisDif = Math.abs(temp1 - temp2);
-				if (thisDif < minDif)
-					minDif = thisDif;
-				temp1 = temp2;
-				temp2 = temp3;
-			}*/
 			minDif = 3000000;
 			maxDif  = 33000000;
 			
@@ -462,14 +310,10 @@ public class DataUsageActivity extends SherlockActivity implements OnTouchListen
 			checkBoundaries();
 			 
 		    graph.setDomainValueFormat(new TimeFormat());
-			
 	    	graph.redraw();
-	    	
 	    	graph.setVisibility(View.VISIBLE);
 	    	d_pb_ll.setVisibility(View.GONE);
-	    	
-	    	//Toast.makeText(DataUsageActivity.this, "Swipe up and down to zoom in and out", Toast.LENGTH_SHORT).show();
-		}
+	    	}
 		@Override
 		protected void onCancelled() {
 			detv.setText(errorMsg);
