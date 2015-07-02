a0aa34c328abb12b2d0055a0f2146ee5f32fb7a1
Chris R <randomnameftw@gmail.com>
2015-07-02 04:56:01 -0500

    Map: update stop data to reflect current capmetro.org data
    Also tweaked the stop parsing a bit to account for lack of newline at the end of
    the files now.

--- src/com/nasageek/utexasutilities/activities/CampusMapActivity.java
+++ src/com/nasageek/utexasutilities/activities/CampusMapActivity.java
@@ -83,8 +83,10 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -699,6 +701,36 @@ public class CampusMapActivity extends BaseActivity implements OnMapReadyCallbac
         buildingIdList.add(q.toUpperCase(Locale.ENGLISH));
     }
 
+    // lifted from http://stackoverflow.com/a/23952928/3214339
+    private String loadAssetAsString(String path) {
+        StringBuilder buffer = new StringBuilder();
+        InputStream in = null;
+        try {
+            in = assets.open(path);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String str;
+            boolean isFirst = true;
+            while ((str = reader.readLine()) != null) {
+                if (isFirst)
+                    isFirst = false;
+                else
+                    buffer.append('\n');
+                buffer.append(str);
+            }
+        } catch (IOException e) {
+            Log.d(CampusMapActivity.class.getSimpleName(), "Error opening asset");
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    Log.d(CampusMapActivity.class.getSimpleName(), "Error closing asset");
+                }
+            }
+        }
+        return buffer.toString();
+    }
+
     /**
      * Displays a route as a set of stop markers and polylines
      *
@@ -728,21 +760,16 @@ public class CampusMapActivity extends BaseActivity implements OnMapReadyCallbac
             // get the results of the parse, null on error
             Deque<Placemark> navData = navSaxHandler.getParsedData();
             drawPath(navData, BURNT_ORANGE);
-            BufferedInputStream bis = new BufferedInputStream(assets.open("stops/"
-                    + stops_al.get(stops_al.indexOf(routeid + "_stops.txt"))));
-            int b;
-            StringBuilder stopData = new StringBuilder();
-            do {
-                b = bis.read();
-                stopData.append((char) b);
-            } while (b != -1);
-            String[] stops = stopData.toString().split("\n");
+
+            String stopPath = "stops/" + stops_al.get(stops_al.indexOf(routeid + "_stops.txt"));
+            String stopData = loadAssetAsString(stopPath);
+            String[] stops = stopData.split("\n");
 
             // clear the stops from the old route
             shownStops.clearMarkers();
 
-            for (int x = 0; x < stops.length - 1; x++) {
-                String data[] = stops[x].split("\t");
+            for (String stop : stops) {
+                String data[] = stop.split("\t");
                 Double lat = Double.parseDouble(data[0].split(",")[0].trim());
                 Double lng = Double.parseDouble(data[0].split(",")[1].trim());
                 String title = data[1];
