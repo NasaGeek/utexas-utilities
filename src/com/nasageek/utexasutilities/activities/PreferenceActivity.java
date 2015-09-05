1600536b5844592507933d3dd45e91e02eb0cc6d
Chris R <randomnameftw@gmail.com>
2015-09-05 01:40:35 -0400

    Enable home affordance in PreferenceActivity

--- src/com/nasageek/utexasutilities/activities/PreferenceActivity.java
+++ src/com/nasageek/utexasutilities/activities/PreferenceActivity.java
@@ -13,5 +13,6 @@ public class PreferenceActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
