0519984dfab1835a9c9123fb2b6a3a42fd45f097
Chris R <randomnameftw@gmail.com>
2018-09-25 23:59:00 -0500

    Properly support newest version of LeakCanary

--- app/src/main/java/com/nasageek/utexasutilities/UTilitiesApplication.java
+++ app/src/main/java/com/nasageek/utexasutilities/UTilitiesApplication.java
@@ -51,6 +51,11 @@ public class UTilitiesApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean sendCrashes = sharedPreferences.getBoolean("sendcrashes", false);
         Crashlytics crashlytics = new Crashlytics.Builder()
