72bf9c8a2412beea3c1f0ee50c75ce951eddb574
Chris R <randomnameftw@gmail.com>
2018-11-03 01:15:37 -0500

    Maps: Manage building db with library
    My code was buggy, and heyo look there's this library that does exactly
    what I need.

--- app/src/main/java/com/nasageek/utexasutilities/BuildingDatabase.java
+++ app/src/main/java/com/nasageek/utexasutilities/BuildingDatabase.java
@@ -4,98 +4,30 @@ package com.nasageek.utexasutilities;
 import android.app.SearchManager;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteCursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.preference.PreferenceManager;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
-public class BuildingDatabase extends SQLiteOpenHelper {
+public class BuildingDatabase extends SQLiteAssetHelper {
 
-    private Context mContext;
-    static final String DB_NAME = "buildings";
-    static final String TABLE_NAME = "buildings";
+    private static final String DB_NAME = "buildings.db";
+    // V1 Initial building list
+    // V2 Added all garages, Belo, a few dorms I missed. Should be the
+    // entire official building list now
+    // V3 CLA - Liberal Arts Building
+    // V4 POB, GDC - POB is ACES but renamed, leave them both in there
+    // V5 Add INT, ESS, CTR, CS3, CS4, CRB, CCF; remove ESB, RAS, RRN;
+    //      rename GIA to GIAC
+    // V6 Re-scrape UT buildings off of maps.utexas.edu (see scripts/ dir for more info)
+    //      building location info is also stored in database now instead of kml;
+    private static final int DB_VERSION = 6;
     private static final String KEY_SUGGEST_COLUMN_TEXT_1 = SearchManager.SUGGEST_COLUMN_TEXT_1;
     private static final String KEY_SUGGEST_COLUMN_TEXT_2 = SearchManager.SUGGEST_COLUMN_TEXT_2;
 
     public BuildingDatabase(Context con) {
-        super(con, DB_NAME, null, 1);
-        mContext = con;
+        super(con, DB_NAME, null, DB_VERSION);
+        setForcedUpgrade();
     }
-
-    public void createDataBase() throws IOException {
-        if (!databaseExists()) {
-            try {
-                copyDataBase();
-                PreferenceManager.getDefaultSharedPreferences(mContext)
-                        .edit().putInt("buildingdbversion", 6).apply();
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new IOException("Error copying building database");
-            }
-        }
-    }
-
-    private void copyDataBase() throws IOException {
-        // Open your local db as the input stream
-        InputStream myInput = mContext.getAssets().open(DB_NAME);
-
-        // Path to the just created empty db
-        String outFileName = mContext.getDatabasePath(DB_NAME).getPath();
-
-        SQLiteDatabase.openOrCreateDatabase(outFileName, null).close();
-        // Open the empty db as the output stream
-        OutputStream myOutput = new FileOutputStream(outFileName);
-
-        // transfer bytes from the inputfile to the outputfile
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = myInput.read(buffer)) > 0) {
-            myOutput.write(buffer, 0, length);
-        }
-
-        // Close the streams
-        myOutput.flush();
-        myOutput.close();
-        myInput.close();
-
-    }
-
-    public SQLiteDatabase openDatabase() throws SQLException {
-        String dbPath = mContext.getDatabasePath(DB_NAME).getPath();
-        return SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READONLY);
-    }
-
-    /**
-     * Check if the database already exist to avoid re-copying the file each
-     * time you open the application.
-     * 
-     * @return true if it exists, false if it doesn't
-     */
-    private boolean databaseExists() {
-        SQLiteDatabase checkDB = null;
-        try {
-            checkDB = openDatabase();
-        } catch (SQLiteException e) {
-            // database does't exist yet.
-        }
-        if (checkDB != null) {
-            checkDB.close();
-        }
-        return checkDB != null;
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) { }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
 
     public Cursor query(String tablename, String[] columns, String selection,
             String[] selectionArgs, String groupBy, String having, String orderBy) {
--- app/src/main/java/com/nasageek/utexasutilities/BuildingProvider.java
+++ app/src/main/java/com/nasageek/utexasutilities/BuildingProvider.java
@@ -5,15 +5,13 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-
-import java.io.IOException;
 
 public class BuildingProvider extends ContentProvider {
 
     private BuildingDatabase bdb;
 
+    private static final String TABLE_NAME = "buildings";
     public static final Uri CONTENT_URI = Uri
             .parse("content://" + BuildConfig.APPLICATION_ID + ".buildingprovider");
 
@@ -34,28 +32,8 @@ public class BuildingProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        // V1 Initial building list
-        // V2 Added all garages, Belo, a few dorms I missed. Should be the
-        // entire official building list now
-        // V3 CLA - Liberal Arts Building
-        // V4 POB, GDC - POB is ACES but renamed, leave them both in there
-        // V5 Add INT, ESS, CTR, CS3, CS4, CRB, CCF; remove ESB, RAS, RRN;
-        //      rename GIA to GIAC
-        // V6 Re-scrape UT buildings off of maps.utexas.edu (see scripts/ dir for more info)
-        //      building location info is also stored in database now instead of kml
 
-        if (PreferenceManager.getDefaultSharedPreferences(this.getContext()).getInt(
-                "buildingdbversion", 1) < 6) {
-            this.getContext().deleteDatabase(BuildingDatabase.DB_NAME);
-        }
         bdb = new BuildingDatabase(this.getContext());
-        try {
-            bdb.createDataBase();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        bdb.close();
-        bdb.openDatabase();
         return true;
     }
 
@@ -66,7 +44,7 @@ public class BuildingProvider extends ContentProvider {
         if (selectionArgs != null) {
             mySelection = selectionArgs[0];
         }
-        return bdb.query(BuildingDatabase.TABLE_NAME, projection, mySelection, null, null, null, sortOrder);
+        return bdb.query(TABLE_NAME, projection, mySelection, null, null, null, sortOrder);
 
     }
 
